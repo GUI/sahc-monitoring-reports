8cdf887375f78eccd9693ec40511b53ef88825a0
Nick Muerdter <12112+GUI@users.noreply.github.com>
2022-10-09 20:10:53 -0600

    Improve browser cache handling of attachments to prevent queries.

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,5 +1,7 @@
 class AttachmentsController < ApplicationController
   def download
+    fresh_when(strong_etag: param_etag)
+
     set_rack_response ApplicationUploader.download_response(request.env)
   end
 
@@ -10,9 +12,16 @@ class AttachmentsController < ApplicationController
     self.headers.merge!(headers)
 
     if status == 200
+      self.headers["ETag"] = param_etag
       self.headers["Cache-Control"] = "private, no-cache, max-age=31536000, immutable"
     end
 
     self.response_body = body
+  end
+
+  def param_etag
+    # The URL parameter is unique if the data changes at all, so we can use it
+    # as the etag.
+    @param_etag ||= Digest::SHA256.hexdigest(params[:rest])[0, 20]
   end
 end
