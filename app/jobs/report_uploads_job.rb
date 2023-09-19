46a946e60c46e5f1f35d2ef028ab061e3bbbd6cd
Nick Muerdter <12112+GUI@users.noreply.github.com>
2023-09-19 08:14:10 -0600

    Debugging.

--- app/jobs/report_uploads_job.rb
+++ app/jobs/report_uploads_job.rb
@@ -6,12 +6,20 @@ class ReportUploadsJob < ApplicationJob
       RequestStore.store[:current_user_email] = current_user_email
 
       upload_uuids.each do |uuid|
+        Rails.logger.info("BEFORE UPLOAD FIND: #{uuid.inspect}")
         upload = Upload.find_by!(:uuid => uuid)
+        Rails.logger.info("AFTER UPLOAD FIND: #{uuid.inspect}")
         upload.build_photos.each do |photo|
+          Rails.logger.info("INSIDE BUILD PHOTOS EACH: #{uuid.inspect}")
+          Rails.logger.info("INSIDE BUILD PHOTOS EACH: #{photo.inspect}")
           photo.report_id = report.id
+          Rails.logger.info("BEFORE PHOTO SAVE: #{uuid.inspect}")
           photo.save!
+          Rails.logger.info("AFTER PHOTO SAVE: #{uuid.inspect}")
         end
+        Rails.logger.info("BEFORE UPLOAD DESTROY: #{uuid.inspect}")
         upload.destroy
+        Rails.logger.info("AFTER UPLOAD DESTROY: #{uuid.inspect}")
       end
     end
 
